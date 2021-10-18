1c14c2cfae6e59d2f16034770562e93af5ecc05b
Neil Earlam <neil.earlam2019@outlook.com>
2021-10-18 18:50:07 +0100

    Created the PackOfCardsCreator and associated tests for the creation of a Pack of Cards

---
+++ CardGame/CardGame/CardGame.Domain/PackOfCardsCreator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using CardGame.Domain.Enums;
+using CardGame.Domain.Interfaces;
+
+namespace CardGame.Domain
+{
+    public class PackOfCardsCreator : IPackOfCardsCreator
+    {
+        public IPackOfCards Create()
+        {
+            var cards = new List<ICard>();
+
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Value value in Enum.GetValues(typeof(Value)))
+                {
+                    cards.Add(new Card(suit, value));
+                }
+            }
+
+            return new PackOfCards(cards.ToArray());
+        }
+    }
+}
---
+++ CardGame/CardGame/CardGame.Tests/PackOfCardsCreatorTests/GivenAPackOfCardsIsNeeded.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using CardGame.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CardGame.Tests.PackOfCardsCreatorTests
+{
+    [TestClass]
+    public class GivenAPackOfCardsIsNeeded
+    {
+        [TestMethod]
+        public void WhenCreateIsCalled_ThenItShouldCreateAPackOfCardsWith52UniqueCardsInIt()
+        {
+            // Arrange
+            var packOfCardsCreatorUnderTest = new PackOfCardsCreator();
+
+            // Act
+            var packOfCards = packOfCardsCreatorUnderTest.Create();
+            var packOfCardsDistinct = packOfCards.Distinct().ToList();
+
+            // Assert
+            Assert.IsNotNull(packOfCards);
+            Assert.AreEqual(52, packOfCards.Count);
+            Assert.AreEqual(52, packOfCardsDistinct.Count);
+        }
+    }
+}
