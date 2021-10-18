66f561f2c55328b14f9f7a510a2f88ffb60c28f3
Neil Earlam <neil.earlam2019@outlook.com>
2021-10-18 17:53:48 +0100

    Card concrete model created and tests verifying the Equals method created and passing

---
+++ CardGame/CardGame/CardGame.Domain/Card.cs
@@ -0,0 +1,34 @@
+﻿using CardGame.Domain.Enums;
+using CardGame.Domain.Interfaces;
+
+namespace CardGame.Domain
+{
+    public class Card : ICard
+    {
+        public Suit Suit { get; set; }
+        public Value Value { get; set; }
+
+        public Card(Suit suit, Value value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+
+        public bool Equals(ICard other)
+        {
+            var result = true;
+
+            if (Suit != other.Suit)
+            {
+                result = result && false;
+            }
+
+            if (Value != other.Value)
+            {
+                result = result && false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CardGame/CardGame/CardGame.Tests/CardTests/GivenTwoCardsAreBeingCompared.cs
@@ -0,0 +1,67 @@
+﻿using CardGame.Domain;
+using CardGame.Domain.Enums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CardGame.Tests.CardTests
+{
+    [TestClass]
+    public class GivenTwoCardsAreBeingCompared
+    {
+        [TestMethod]
+        public void WhenBothSuitAndValuesAreEqual_ThenEqualsShouldReturnTrue()
+        {
+            // Arrange
+            var cardOne = new Card(Suit.Clubs, Value.Ace);
+            var cardTwo = new Card(Suit.Clubs, Value.Ace);
+
+            // Act
+            var result = cardOne.Equals(cardTwo);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void WhenTheSuitIsDifferentButValueIsEqual_ThenEqualsReturnsFalse()
+        {
+            // Arrange
+            var cardOne = new Card(Suit.Clubs, Value.Ace);
+            var cardTwo = new Card(Suit.Diamonds, Value.Ace);
+
+            // Act
+            var result = cardOne.Equals(cardTwo);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void WhenTheSuitIsEqualButTheValueIsDifferent_ThenEqualsReturnsFalse()
+        {
+            // Arrange
+            var cardOne = new Card(Suit.Clubs, Value.Ace);
+            var cardTwo = new Card(Suit.Clubs, Value.Eight);
+
+            // Act
+            var result = cardOne.Equals(cardTwo);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void WhenBothTheSuitAndValuesAreDifferent_ThenEqualsShouldReturnFalse()
+        {
+
+            // Arrange
+            var cardOne = new Card(Suit.Clubs, Value.Ace);
+            var cardTwo = new Card(Suit.Diamonds, Value.Eight);
+
+            // Act
+            var result = cardOne.Equals(cardTwo);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
